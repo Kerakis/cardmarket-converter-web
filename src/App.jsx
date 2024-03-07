8eea9ffec136ec513977298fb171f9baba96778c
Rob Upchurch <rlupchurch3@gmail.com>
2024-03-07 09:47:09 -0500

    Update HTML title and favicon, add footer to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import FetchData from './FetchData';
 
 function App() {
+  const year = new Date().getFullYear();
   return (
     <div className='App'>
       <FetchData />
+      <footer className='flex-shrink-0 mt-8 text-sm text-center lg:fixed lg:m-1 lg:bottom-0 lg:right-1'>
+        <p>
+          Made with <span className='font-sans'>&#9749;</span> by
+          <a
+            href='https://github.com/Kerakis'
+            target='_blank'
+            rel='noopener noreferrer'>
+            &nbsp;Kerakis&nbsp;
+          </a>
+          © {year}
+        </p>
+      </footer>
     </div>
   );
 }
